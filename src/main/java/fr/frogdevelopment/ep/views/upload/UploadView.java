07be6af2cb8788b424347d9df61d0b22c8272869
LE GALL Benoît <legall.benoit@gmail.com>
2020-02-16 23:30:42 +0100

    feat(vaadin): upload planning (implementation from Xls2Glide)

--- src/main/java/fr/frogdevelopment/ep/views/upload/UploadView.java
+++ src/main/java/fr/frogdevelopment/ep/views/upload/UploadView.java
@@ -1,34 +1,40 @@
 package fr.frogdevelopment.ep.views.upload;
 
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.FinishedEvent;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import fr.frogdevelopment.ep.implementation.xls.ReadXls;
 import fr.frogdevelopment.ep.views.MainView;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "upload", layout = MainView.class)
 @PageTitle("Upload")
 //@Secured("ROLE_Admin")
 public class UploadView extends VerticalLayout {
 
-    @Autowired
-    private ReadXls readXls;
+    private final transient ReadXls readXls;
+    private final MemoryBuffer buffer;
 
-    public UploadView() {
+    public UploadView(ReadXls readXls) {
+        this.readXls = readXls;
+
         var label = new Label("Charge les données à partir de l'Xls");
         add(label);
 
-//        var buffer = new FileBuffer();
-        var buffer = new MemoryBuffer();
+        buffer = new MemoryBuffer();
         var upload = new Upload(buffer);
         upload.setAcceptedFileTypes(".xls", ".xlsx", ".xlsm");
-        upload.addFinishedListener(e -> readXls.call(buffer.getInputStream()));
+        upload.addFinishedListener(readXls());
 
         add(upload);
+    }
+
+    private ComponentEventListener<FinishedEvent> readXls() {
+        return e -> this.readXls.call(buffer.getInputStream());
     }
 
 }
